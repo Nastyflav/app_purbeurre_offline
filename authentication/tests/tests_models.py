31b5f4c386419dfe03485a2acfb8aecfeb630e99
Flavien <flavien.murail@gmail.com>
2020-05-14 16:58:18 +0200

    Auth models tests are ok + PEP8

--- authentication/tests/tests_models.py
+++ authentication/tests/tests_models.py
@@ -7,9 +7,9 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 """
 
-from django.test import TestCase, Client
-from django.urls import reverse
+from django.test import TestCase
 from django.db import IntegrityError
+
 from authentication.models import User
 
 
@@ -17,13 +17,21 @@ class TestModels(TestCase):
     """To test the authentication models"""
     def setUp(self):
         """ Create a temp user to perform tests"""
-        self.client = Client()
-        self.login_url = reverse('authentication:login')
-        self.logout_url = reverse('authentication:logout')
-        self.profile_url = reverse('authentication:profile')
-        self.user = User.objects.create(email='user@test.dj')
+        self.user = User.objects.create(
+            email='user@test.dj', first_name='User', last_name='Django')
         self.user.set_password('supertest2020')
-        self.user.save()
+
+    def test_first_name_max_length(self):
+        """To check the first name lenght"""
+        user = User.objects.get(id=1)
+        max_length = user._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 40)
+
+    def test_last_name_max_length(self):
+        """To check the last name lenght"""
+        user = User.objects.get(id=2)
+        max_length = user._meta.get_field('last_name').max_length
+        self.assertEquals(max_length, 40)
 
     def test_signup_and_str_return_when_user_creation_is_ok(self):
         """Test when the creation of new user is authorized"""
@@ -35,3 +43,13 @@ class TestModels(TestCase):
         user = User.objects.get(email=new_user_email)
         self.assertIsInstance(user, User)
         self.assertEqual(str(user), new_user_email)
+
+    def test_signup_when_user_already_exists(self):
+        """Check if a similar user or a similar password already exists"""
+        try:
+            user = User.objects.create(email='user@test.dj')
+            user.set_password('supernouveau')
+            user.save()
+        except IntegrityError:
+            user = False
+        self.assertFalse(user)
