6a0118f843bc8ff69e99c162f36fedb31e1c6f76
Flavien <flavien.murail@gmail.com>
2020-05-14 18:56:19 +0200

    Urls tests for save are OK + PEP8

--- save/tests/tests_urls.py
+++ save/tests/tests_urls.py
@@ -10,3 +10,23 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 from django.test import TestCase
 from django.urls import reverse
 
+
+class TestUrls(TestCase):
+    """To test the save urls file"""
+    def setUp(self):
+        """Paths to the urls"""
+        self.sub_url = reverse('save:substitution', args=[10])
+        self.save_url = reverse('save:save')
+        self.fav_url = reverse('save:favorites')
+
+    def test_substitution_page_url(self):
+        """To check the substitution url when requested"""
+        self.assertEqual(self.sub_url, '/save/substitution/10')
+
+    def test_saving_page_url(self):
+        """To check the saving url when requested"""
+        self.assertEqual(self.save_url, '/save/save/')
+
+    def test_favorites_page_url(self):
+        """To check the favorites url when requested"""
+        self.assertEqual(self.fav_url, '/save/favorites/')
