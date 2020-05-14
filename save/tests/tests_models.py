92b3727d399b5e136826aaa60762947c2d146092
Flavien <flavien.murail@gmail.com>
2020-05-14 18:48:32 +0200

    Added a tests folder to the save app

---
+++ save/tests/tests_models.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-05-14
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
+from django.test import TestCase
+from django.db import IntegrityError
+
+from authentication.models import User
