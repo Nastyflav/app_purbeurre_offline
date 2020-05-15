281041669677652adf64f6d9b12ca32eeb4e2a89
Flavien <flavien.murail@gmail.com>
2020-05-15 16:47:27 +0200

    Updated chrome test

--- tests/test_chrome.py
+++ tests/test_chrome.py
@@ -13,17 +13,14 @@ from django.test import TestCase
 
 class TestChrome(TestCase):
     """To test the user story using Chrome browser"""
-    def setUp(self):
+    def setUp(self)
         self.browser = webdriver.Chrome(executable_path='./chromedriver')
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test(self):
         self.browser.get('https://purbeurreflavien.herokuapp.com/')
-
-        # Elle remarque que le mot Planning
-        # figure dans le titre de la page
         self.assertIn('Beurre', self.browser.title)  #
         self.fail('Test terminé !')
