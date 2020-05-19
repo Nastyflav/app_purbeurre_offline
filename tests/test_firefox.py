66c7fa6974e0c46e667599b5638fd7cb082497f4
Flavien <flavien.murail@gmail.com>
2020-05-19 15:48:32 +0200

    Firefox tests are OK + PEP8

--- tests/test_firefox.py
+++ tests/test_firefox.py
@@ -10,6 +10,53 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 from django.contrib.staticfiles.testing import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from authentication.models import User
+from search.models import Category, Product
+
+
+def temp_user_creation():
+    user = User.objects.create(email='selenium@user.test')
+    user.set_password('test2020')
+    user.save()
+
+
+def db_init():
+    data = Category(name="Pate à tartiner")
+    data.save()
+
+    data = Product(
+        name="Ovomaltine",
+        category_id=Category.objects.get(name="Pate à tartiner"),
+        store="Leclerc, BioCoop",
+        nutriscore="a",
+        barcode="0189654870000",
+        url="https://ovomaltine.fr",
+        image="https://ovomaltine.fr/photo.jpg",
+        lipids_for_100g="4.59",
+        saturated_fats_for_100g="0.02",
+        sugars_for_100g="1.54",
+        salt_for_100g="3.25",
+    )
+    data.save()
+
+    data = Product(
+        name="Nutella",
+        category_id=Category.objects.get(name="Pate à tartiner"),
+        store="Auchan",
+        nutriscore="b",
+        barcode="012232370000",
+        url="https://nutella.fr",
+        image="https://nutella.fr/photo.jpg",
+        lipids_for_100g="1.64",
+        saturated_fats_for_100g="0.33",
+        sugars_for_100g="2.20",
+        salt_for_100g="1.06",
+    )
+    data.save()
 
 
 class TestFirefox(LiveServerTestCase):
@@ -19,11 +66,14 @@ class TestFirefox(LiveServerTestCase):
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
+        cls.selenium.maximize_window()
+        temp_user_creation()
+        db_init()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super().tearDownClass()
+    # @classmethod
+    # def tearDownClass(cls):
+    #     cls.selenium.quit()
+    #     super().tearDownClass()
 
     def test_login(self):
         """Test when the user wants to log in"""
@@ -34,3 +84,34 @@ class TestFirefox(LiveServerTestCase):
         password_input.send_keys('test2020')
         submit = self.selenium.find_element_by_id("submit-button")
         submit.send_keys(Keys.RETURN)
+
+    def test_search_product_details(self):
+        """To test when the user wants to search a prod and its details"""
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        query_input = self.selenium.find_element_by_id("query")
+        query_input.send_keys('nutella')
+        search = self.selenium.find_element_by_id("search-btn")
+        search.send_keys(Keys.RETURN)
+        product = WebDriverWait(self.selenium, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='details-link']")))
+        product.click()
+
+    def test_save_product(self):
+        """To test when the user wants to save a product"""
+        self.test_login()
+        query_input = self.selenium.find_element_by_id("query")
+        query_input.send_keys('nutella')
+        search = self.selenium.find_element_by_id("search-btn")
+        search.send_keys(Keys.RETURN)
+        product = WebDriverWait(self.selenium, 30).until(
+            EC.element_to_be_clickable((By.ID, "product-title")))
+        product.click()
+        substitute = WebDriverWait(self.selenium, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='save-btn']")))
+        substitute.click()
+
+    def test_logout(self):
+        self.test_login()
+        logout = WebDriverWait(self.selenium, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='logout']")))
+        logout.click()
