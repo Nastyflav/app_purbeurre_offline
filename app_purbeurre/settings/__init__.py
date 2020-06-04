6f2a99631bcea3fe4e138bf5dff53bde1af8e0ff
Flavien <flavien.murail@gmail.com>
2020-06-04 17:31:09 +0200

    Cleaned the database in settings

--- app_purbeurre/settings/__init__.py
+++ app_purbeurre/settings/__init__.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'app_purbeurre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_purbeurre',
-        'USER': 'admin_purbeurre',
-        'PASSWORD': 'purbeurre',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
