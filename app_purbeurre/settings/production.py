de111ad9749ed5a74bda77578f14b8d6618c3723
Flavien <flavien.murail@gmail.com>
2020-06-09 12:08:52 +0200

    Added Sentry to production settings

--- app_purbeurre/settings/production.py
+++ app_purbeurre/settings/production.py
@@ -1,5 +1,8 @@
 from . import *
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 SECRET_KEY = '#5=%f%e^58fu_9w9ynf*9=t&!0+)(gw6htzprps)fjj4!4zzuj'
 DEBUG = False
@@ -15,3 +18,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+sentry_sdk.init(
+    dsn="https://b902192fca2c4187bc9f286223a6bec9@o401185.ingest.sentry.io/5269916",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
